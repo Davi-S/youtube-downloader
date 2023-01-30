8a0cb0d35bcd6af64f638b2e10772b9d956f1584
Davi S <67160664+Davi-S@users.noreply.github.com>
2023-01-30 14:35:28 -0300

    2.0
    mass change

--- downloader.py
+++ downloader.py
@@ -1,98 +1,60 @@
 import pytube
 import io
 import moviepy.editor
-import threading
 
 
-class YoutubeDowloader:
-    def __init__(self, url, mode, quality, name, extention, destination) -> None:
-        print('Starting downloader')
-        self.url = url
-        self.mode = mode
-        self.quality = quality
-        self.name = name
-        self.extention = extention
-        self.destination = destination
-        
-        self.yt: list[pytube.YouTube] = self._create_video_obj()
-
-
-    def _create_video_obj(self) -> list[pytube.YouTube]:
-        """Return a list containing one or more pytube.YouTube elemenst
-
-        Returns:
-            list[pytube.YouTube]: list of pytube.YouTube objects
-        """
-        if "watch?v=" in self.url:
-            return [pytube.YouTube(self.url)]  # Return as a list
-
-        elif "playlist?list=" in self.url:
-            return list(pytube.Playlist(self.url).videos)
-
-
-    def get_av_stream(self, yt: pytube.YouTube) -> tuple[pytube.Stream, pytube.Stream]:
-        """Get the audio and video stream matching the class atributes
+def filter_stream_query(stream_query: pytube.StreamQuery, stream_type: str, quality: str) -> pytube.Stream:
+        """Get the audio/video stream from streamquery that matches the class atributes (quality and mode)
 
         Args:
             yt (pytube.YouTube): The youtube object
 
         Returns:
-            tuple[pytube.Stream, pytube.Stream]: Audio and video streams respectively
+            pytube.Stream: Video stream
         """
-        # get streams
-        stream = yt.streams
-
-        # get the rigth steam for the the rigth mode:
-        stream_audio = stream.filter(only_audio=True) if self.mode == 'audio' else pytube.StreamQuery([])  # empty stream query
-        stream_video = stream.filter(only_video=True) if self.mode == 'video' else pytube.StreamQuery([])
-
-        # sort the streams
-        stream_audio = stream_audio.order_by('abr')
-        stream_video = stream_video.order_by('resolution')
-
-        # get the stream acordiong to the determined quality
-        stream_audio = stream_audio.last() if self.quality == 'high' else (None if IndexError else stream_audio[len(stream_audio) // 2]) if self.quality == 'medium' else stream_audio.first()
-        stream_video = stream_video.last() if self.quality == 'high' else (None if IndexError else stream_video[len(stream_video) // 2]) if self.quality == 'medium' else stream_video.first()
+        # get the rigth mode and sort
+        streams_mode = stream_query.filter(only_audio=stream_type == 'audio', only_video=stream_type == 'video')
+        stream_sort = streams_mode.order_by('abr' if stream_type == 'audio' else 'resolution')
         
-        return stream_audio, stream_video
+        if quality == 'high':
+            stream = stream_sort.last()
         
-    def merge_streams(self, stream_audio: pytube.Stream, stream_video: pytube.Stream) -> moviepy.editor.VideoFileClip:
-        """Merge togheter audio and video streams into a moviepy.editor.VideoFileClip object 
-
-        Args:
-            stream_audio (pytube.Stream): Audio stream
-            stream_video (pytube.Stream): Video steam
-
-        Returns:
-            moviepy.editor.VideoFileClip: Video with audio
-        """
-        # Create a BytesIO object from the audio stream data
-        audio_data = io.BytesIO()
-        stream_audio.download(audio_data)
-        audio_data.seek(0)
+        elif quality == 'medium':
+            stream = stream_sort[len(stream_sort) // 2]
         
-        # Create a BytesIO object from the video stream data
-        video_data = io.BytesIO()
-        stream_video.download(video_data)
-        video_data.seek(0)
-
-        # Create an audio clip object
-        audio = moviepy.editor.AudioFileClip(audio_data)
-        
-        # Create a video clip object
-        video = moviepy.editor.VideoFileClip(video_data)
-
-        # Set the audio of the video clip
-        video.audio = audio
-
-        # Return the video with the audio
-        return video
+        elif quality == 'low':
+            stream = stream_sort.first()
+        return stream
     
-    def download_file(self, video_file: moviepy.editor.VideoFileClip):
-        video_file.write_videofile(filename=f'{self.destination}/{self.name}.{self.extention}')
-    
-    def dowload_stream(self, stream: pytube.Stream):
-        stream.download(output_path=self.destination, filename=f"{self.name}.{self.extention}")
 
-    def start(self):
-        pass # todo
+def merge_streams(stream_audio: pytube.Stream, stream_video: pytube.Stream) -> moviepy.editor.VideoFileClip:
+    """Merge togheter audio and video streams into a moviepy.editor.VideoFileClip object 
+
+    Args:
+        stream_audio (pytube.Stream): Audio stream
+        stream_video (pytube.Stream): Video steam
+
+    Returns:
+        moviepy.editor.VideoFileClip: Video with audio
+    """
+    audio = moviepy.editor.AudioFileClip(stream_audio.url)
+    video = moviepy.editor.VideoFileClip(stream_video.url)
+
+    # Set the audio of the video clip and return it
+    video.audio = audio
+    return video
+
+
+def _download_file(file: moviepy.editor.VideoFileClip, destination: str, name: str, extention: str='mp4') -> None:
+    file.write_videofile(filename=f'{destination}/{name}.{extention}', logger=None)
+
+
+def _dowload_stream(stream: pytube.Stream, destination: str, name: str, extention: str) -> None:
+    stream.download(output_path=destination, filename=f"{name}.{extention}")
+
+
+def dowload_any(file, destination: str, name: str, extention: str) -> None:
+    if type(file) == pytube.Stream:
+        _dowload_stream(file, destination, name, extention)
+    else:
+        _download_file(file, destination, name, extention)
--- main.py
+++ main.py
@@ -1,33 +1,69 @@
 # IMPORTS #
+from downloader import filter_stream_query, merge_streams, dowload_any
 import argparse
 import os
 import re
-from downloader import YoutubeDowloader
+import pytube
 
 
-def validate_destination_path(path: str) -> str:
+def format_destination_path(path: str) -> str:
     return path.rstrip("/\\")
 
 
-def dowloads_path():
-        folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
-        return folder if os.path.exists(folder) else None
+def dowloads_path() -> str:
+    folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
+    return folder if os.path.exists(folder) else None
     
     
-def validate_youtube_url(url: str) -> bool:
+def validate_youtube_url(url: str) -> str:
     video_pattern = r"^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/watch\?v=[a-zA-Z0-9\-_]+$"
     playlist_pattern = r"^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/playlist\?list=[a-zA-Z0-9\-_]+$"
     match_video = re.match(video_pattern, url)
     match_playlist = re.match(playlist_pattern, url)
     if not (match_video or match_playlist):
-        raise argparse.ArgumentTypeError("This url is invalid. A valid url should look like this: https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        raise argparse.ArgumentTypeError("This url is invalid. A valid url should look like this: https://www.youtube.com/watch?v=dQw4w9WgXcQ")  
     return url
 
 
-def main(args: argparse.Namespace):
-    YoutubeDowloader(**vars(args)).get_video_stream()
-    # YoutubeDowloader(vars(args)).download()
-    print('Done!')
+def download_single(yt: pytube.YouTube, stream_type: str, quality: str, destination) -> str:
+    print(f'Colecting video {yt.title}')
+    query = yt.streams
+
+    print('Setting download preferences')
+    if stream_type == 'av':
+        stream = (filter_stream_query(query, 'audio', quality),
+                  filter_stream_query(query, 'video', quality))
+    else:
+        stream = (filter_stream_query(query, stream_type, quality), None)
+            
+    file = merge_streams(*stream) if stream[1] is not None else stream[0]
+    
+    name = stream[0].default_filename
+    extention = 'mp3' if stream_type == 'audio' else 'mp4'
+    
+    print('Downloading file')
+    dowload_any(file, destination, name, extention)
+    print(f'Downloaded {yt.title}')
+    return f'{destination}/{name}/{extention}'
+
+
+def main(args: argparse.Namespace) -> int:
+    print('Starting...')
+    
+    download = None
+    
+    if "watch?v=" in args.url:  # single video
+        print('Video detected')
+        download = download_single(pytube.YouTube(args.url), args.mode, args.quality, args.destination)
+
+    else:
+        print('Playlist detected')
+        playlist = pytube.Playlist(args.url)
+        for yt in playlist.videos:
+            download = download_single(yt, args.mode, args.quality, args.destination)
+    
+    print(f'Done! Download in: {download}')
+    return 0
 
 
 if __name__ == '__main__':    
@@ -36,10 +72,8 @@ if __name__ == '__main__':
 
     parser.add_argument('url', type=validate_youtube_url, help='The url of the video, shorts, or playlist')
     parser.add_argument('-m', '--mode', type=str, help='Just audio; just video; or audio and video', metavar='', choices=['audio', 'video', 'av'], default='av')
-    parser.add_argument('-q', '--quality', type=str, help='Preferred quality of the dowload', metavar='', choices=['high', 'medium', 'low'], default='high')
-    parser.add_argument('-n', '--name', type=str, help='The name that the file will be saved', metavar='')
-    parser.add_argument('-e', '--extention', type=str, help='The file extention, like "mp3" or "mp4"', metavar='')
-    parser.add_argument('-d', '--destination', type=validate_destination_path, help='Path to here to dowload the file', metavar='', default=dowloads_path() or os.getcwd())
+    parser.add_argument('-q', '--quality', type=str, help='Preferred quality of the dowload', metavar='', choices=['high', 'medium', 'low'], default='medium')
+    parser.add_argument('-d', '--destination', type=format_destination_path, help='Path to here to dowload the file', metavar='', default=dowloads_path() or os.getcwd())
     
     args = parser.parse_args()
     main(args)
